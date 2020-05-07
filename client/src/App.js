e1bc7134881b3b23a6b7102e9b9d5046fa739835
aileen <44736622+wua3@users.noreply.github.com>
2020-05-07 15:42:08 -0400

    Build the ListTodo component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 // components
 import InputTodo from "./components/InputTodo";
+import ListTodo from "./components/ListTodo";
 
 function App() {
   return (
     <Fragment>
       <div className="container">
         <InputTodo />
+        <ListTodo />
       </div>
     </Fragment>
   );
