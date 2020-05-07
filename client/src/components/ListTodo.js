655c4bb5450cd779221671cf806619da00deeefc
aileen <44736622+wua3@users.noreply.github.com>
2020-05-07 15:52:18 -0400

    Build delete button

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -3,6 +3,19 @@ import React, {Fragment, useEffect, useState} from "react";
 const ListTodos = () => {
     const [todos, setTodos] = useState([]);
 
+    // delete todo function
+    const deleteTodo = async(id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            });
+
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
     const getTodos = async() => {
         try {
             const response = await fetch("http://localhost:5000/todos");
@@ -12,7 +25,7 @@ const ListTodos = () => {
         } catch (err) {
             console.error(err.message);
         }
-    }
+    };
 
     useEffect(() => {
         getTodos();
@@ -29,18 +42,18 @@ const ListTodos = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {/*
-                    <tr>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>john@example.com</td>
-                    </tr>
-                    */}
                     {todos.map(todo => (
-                        <tr>
+                        <tr key={todo.todo_id}>
                             <td>{todo.description}</td>
                             <td>Edit</td>
-                            <td>Delete</td>
+                            <td>
+                                <button 
+                                    className="btn btn-danger" 
+                                    onClick={() => deleteTodo(todo.todo_id)}
+                                >
+                                    Delete
+                                </button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
